844b3fa9be3ad8716d664a16b1dbeeac1e67ed89
Anatoliy <avkindrat2006@gmail.com>
2025-08-08 19:12:36 +0300

    Add second derived class

---
+++ RepairComputer.cpp
@@ -0,0 +1,8 @@
+#include "RepairComputer.h"
+#include "Computer.h"
+
+using namespace std;
+
+RepairComputer::RepairComputer() {};
+
+RepairComputer::~RepairComputer() {};
---
+++ RepairComputer.h
@@ -0,0 +1,14 @@
+#ifndef REPAIRCOMPUTER_H
+#define REPAIRCOMPUTER_H
+
+#include "Computer.h"
+using namespace std;
+
+class RepairComputer
+{
+public:
+	RepairComputer();
+	~RepairComputer();
+};
+
+#endif //REPAIRCOMPUTER_H
