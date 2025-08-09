5b75ee8e23a36f0eb19ddd5eb60e79d1b484bc15
Anatoliy <avkindrat2006@gmail.com>
2025-08-09 20:45:32 +0300

    Rework Set method

--- Computer.cpp
+++ Computer.cpp
@@ -52,33 +52,15 @@ void Computer::SetComputer(
 	int inventoryNumber,
 	int auditoriumNumber)
 {
-  		cout << "write name CPU" ;
-  		cin >> cpu;
-        cpu = m_cpu;
-  		cout << " write name GPU ";
-  		cin >> gpu;
-        gpu = m_gpu;
-    	cout << " write name monitor ";
-    	cin >> monitor;
-	    monitor = m_monitor;
-    	cout << "write type of keyboard";
-    	cin >> keyboard;
-	    keyboard = m_keyboard;
-    	cout << "write have PC CD-ROM?\"if have write 1, else write 0";
-    	cin >> hasCdRom;
-	    hasCdRom = m_hasCdRom;
-    	cout << "write have PC Floppy disk?\"if have write 1, else write 0";
-    	cin >> hasFloppyDisk;
-	    hasFloppyDisk = m_hasFloppyDisk;
-    	cout << "write size of ROM";
-    	cin >> sizeOfRom;
-	    sizeOfRom = m_sizeOfRom;
-    	cout << "write inventory number PC";
-        cin >> inventoryNumber;
-	    inventoryNumber = m_inventoryNumber;
-        cout << "write auditorium number";
-        cin >> auditoriumNumber;
-	    auditoriumNumber = m_auditoriumNumber;
+        m_cpu = cpu;
+        m_gpu = gpu;
+	    m_monitor = monitor;
+	    m_keyboard = keyboard;
+	    m_hasCdRom = hasCdRom;
+	    m_hasFloppyDisk = hasFloppyDisk;
+	    m_sizeOfRom = sizeOfRom;
+	    m_inventoryNumber = inventoryNumber;
+	    m_auditoriumNumber = auditoriumNumber;
 };
 
 //Get-метод
