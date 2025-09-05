812e14db8be90be2220b138167bba29201a92448
Anatoliy <avkindrat2006@gmail.com>
2025-09-05 20:44:02 +0300

    Add new class Exeption

---
+++ Exeption.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Користувач on 05.09.2025.
+//
+
+#include "Exeption.h"
---
+++ Exeption.h
@@ -0,0 +1,16 @@
+//
+// Created by Користувач on 05.09.2025.
+//
+
+#ifndef EXEPTION_H
+#define EXEPTION_H
+
+
+
+class Exeption {
+
+};
+
+
+
+#endif //EXEPTION_H
