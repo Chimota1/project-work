58348958b1da7d06a8660169e1a409410a39b8e8
Anatoliy <avkindrat2006@gmail.com>
2025-09-15 17:42:57 +0300

    Add all include headears to Interface.h file

--- Interface.h
+++ Interface.h
@@ -1,6 +1,13 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include "Computer.h"
+#include <string>
+#include "WorkedComputer.h"
+#include "RepairComputer.h"
+#include <iostream>
+#include <vector>
+#include <memory>
 using namespace std;
 
 class Interface
