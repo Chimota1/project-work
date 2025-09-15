03c55e3fbb25ba0e8150a7a2c1ab2d8bb223dc1d
Anatoliy <avkindrat2006@gmail.com>
2025-09-15 22:00:51 +0300

    add logic of users in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,28 +5,53 @@
 #include "Manager.h"
 #include "Interface.h"
 #include <memory>
+#include <fstream>
 using namespace std;
 
-int main()
+void fileCheck(fstream &file)
 {
+    if (!file.is_open()) throw Exeption("File not found");
+};
+
+int main() {
     bool isRunning = true;
+    bool isAdmin = false;
+    int chooseAccount;
     int k;
+    fstream file("password.txt", ios::in | ios::out | ios::app);
+    string password;
     Manager manager;
 
+    try
+    {
+        fileCheck(file);
+    }
+    catch (const Exeption &e)
+    {
+        cerr << "Exсeption: " << e.what() << endl;
+        return 1;
+    };
+
     cout << "Welcome to the Computer Management System" << endl;
-    cout << "use key for using program" << endl;
+    cout << "Choose account type: 1 - Admin, 2 - User" << endl;
+    cin >> chooseAccount;
     while (isRunning)
     {
-        cin >> k;
-        switch (k)
+        if (chooseAccount == 1 && isAdmin == false)
         {
-            default:
-                cout << "Wrong key, try again" << endl;
+            cout << "You are using Admin account, please enter password" << endl;
+            cin >> password;
+            cin >> k;
+            switch (k)
+            {
+                default:
+                    cout << "Wrong key, try again" << endl;
                 break;
-            case 6:
-                isRunning = false;
+                case 6:
+                    isRunning = false;
                 break;
+            }
         }
+        return 0;
     }
-    return 0;
 };
